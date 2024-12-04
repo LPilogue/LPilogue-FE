15e277429ff2490ffd4010cbf40cfef44db27cd0
1stMourinhoFan <jiyun1215@gachon.ac.kr>
2024-12-05 00:13:41 +0900

    #1 styled: styled-component 형태로 코드 수정, feat: Loading 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Write from './pages/Write';
+import Loading from './components/Loading';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/write" element={<Write />} />
+      <Route path="/loading" element={<Loading />} />
     </Routes>
   );
 }
--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,7 +1,90 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+// Styled Components 정의
+// 가장 큰 Div
+const Container = styled.div`
+  max-width: 390px;
+  width: 390px;
+  margin: 0 auto;
+  border: none;
+  background-color: #f9f2df;
+  user-select: none;
+`;
+
+const Layout = styled.div`
+  display: flex;
+  margin-top: 12px;
+  flex-direction: column;
+  gap: 27px;
+`;
+
+const Button = styled.button`
+  background-color: ${(props) => (props.active ? '#56B7C4' : '#ccc')};
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: ${(props) => (props.active ? 'pointer' : 'not-allowed')};
+  width: 55px;
+  height: 35px;
+  margin-right: 27px;
+  white-space: nowrap;
+  align-self: flex-end;
+`;
+
+const InputDate = styled.input`
+  width: 100%;
+  height: 30px;
+  background-color: #f9f2df;
+  border: none;
+  border-radius: 4px;
+  outline: none;
+  padding: 0px 27px;
+  box-sizing: border-box;
+  pointer-events: ${(props) => (props.disabled ? 'none' : 'auto')};
+`;
+
+const TextareaContainer = styled.div`
+  position: relative;
+  margin: 24px 33px 0px 27px;
+`;
+
+const Textarea = styled.textarea`
+  width: 330px;
+  height: 450px;
+  line-height: 1.25;
+  border: none;
+  outline: none;
+  background-color: #f9f2df;
+  resize: none;
+`;
+
+const TextareaLabel = styled.span`
+  position: absolute;
+  width: 50px;
+  height: 20px;
+  bottom: 15px;
+  right: 8px;
+`;
+
+const Line = styled.hr`
+  width: 340px;
+  border: 0.5px solid #ccc;
+  margin-top: 17px;
+`;
+
 const Write = () => {
+  const today = new Date();
+  const threeDaysAgo = new Date();
+  threeDaysAgo.setDate(today.getDate() - 3);
+  const todayString = today.toISOString().split('T')[0];
+  const threeDaysAgoString = threeDaysAgo.toISOString().split('T')[0];
+
   const [text, setText] = useState('');
-  const [date, setDate] = useState('');
+  const [date, setDate] = useState(todayString);
+
+  const navigate = useNavigate();
 
   const handleTextChange = (event) => {
     setText(event.target.value);
@@ -11,90 +94,41 @@ const Write = () => {
     setDate(event.target.value);
   };
 
-  // 오늘 날짜 및 3일 전 날짜 계산
-  const today = new Date();
-  const threeDaysAgo = new Date();
-  threeDaysAgo.setDate(today.getDate() - 3);
-  const todayString = today.toISOString().split('T')[0];
-  const threeDaysAgoString = threeDaysAgo.toISOString().split('T')[0];
+  const handleButtonClick = () => {
+    if (date && text.length > 0) {
+      navigate('/loading'); // /loading 경로로 이동
+    }
+  };
 
   return (
-    <div
-      style={{
-        padding: '20px',
-        maxWidth: '390px', // 컴포넌트 넓이 고정
-        width: '390px',
-        margin: '0 auto',
-        border: 'none',
-        backgroundColor: '#F9F2DF', // 전체 배경 색상 설정
-        userSelect: 'none',
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column', // 세로로 정렬
-          gap: '15px', // 각 행 사이의 간격
-        }}
-      >
-        <button
-          onClick={() => alert('일기 저장 완료!')}
-          disabled={!date || text.length < 1} // 날짜 선택 및 최소 1글자 입력 시 활성화
-          style={{
-            backgroundColor: date && text.length > 0 ? '#56B7C4' : '#ccc',
-            color: '#fff',
-            border: 'none',
-            borderRadius: '5px',
-            cursor: date && text.length > 0 ? 'pointer' : 'not-allowed',
-            width: '40px',
-            whiteSpace: 'nowrap',
-            alignSelf: 'flex-end',
-          }}
-        >
+    <Container>
+      <Layout>
+        <Button onClick={handleButtonClick} active={text.length > 0}>
           완료
-        </button>
-        <input
+        </Button>
+        <InputDate
           type="date"
           value={date}
           onChange={handleDateChange}
-          max={todayString} // 오늘 날짜까지 선택 가능
-          min={threeDaysAgoString} // 3일 전 날짜부터 선택 가능
-          onKeyDown={(e) => e.preventDefault()} // 키보드를 통한 입력 방지
-          style={{
-            width: '100%',
-            height: '30px',
-            backgroundColor: '#F9F2DF', // 입력 필드 배경 색상
-            border: 'none',
-            borderRadius: '4px',
-            outline: 'none', // 포커스 시 태두리 제거
-          }}
+          max={todayString}
+          min={threeDaysAgoString}
+          onKeyDown={(e) => e.preventDefault()}
         />
-      </div>
+      </Layout>
 
-      <hr style={{ border: '0.5px solid #ccc', margin: '15px 0' }} />
+      <Line />
 
-      <textarea
-        placeholder="일기를 작성해보세요"
-        value={text}
-        onChange={handleTextChange}
-        spellCheck="false"
-        style={{
-          width: '100%',
-          height: '450px',
-          lineHeight: '1.25',
-          border: 'none', // 가장자리 제거
-          outline: 'none', // 포커스 시 외곽선 제거
-          backgroundColor: '#F9F2DF', // 텍스트 영역 배경 색상
-          resize: 'none', // 크기 조절 비활성화
-        }}
-        maxLength={500}
-      />
-      <div
-        style={{ textAlign: 'right', marginBottom: '10px', userSelect: 'none' }}
-      >
-        <span>{text.length}/500</span>
-      </div>
-    </div>
+      <TextareaContainer>
+        <Textarea
+          placeholder="일기를 작성해보세요"
+          value={text}
+          onChange={handleTextChange}
+          spellCheck="false"
+          maxLength={500}
+        />
+        <TextareaLabel>{text.length}/500</TextareaLabel>
+      </TextareaContainer>
+    </Container>
   );
 };
 
