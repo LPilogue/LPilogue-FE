3a71eabb0a54398aaa23a9719c77e73818e918cf
Park yejin <92977621+yezzan9@users.noreply.github.com>
2024-12-16 21:00:18 +0900

    feat: UI 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Write from './pages/Write';
 import Loading from './components/Loading';
+import Recommend from './pages/Recommend';
+import Representative from './pages/Representative';
+import Result from './pages/Result';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/write" element={<Write />} />
       <Route path="/loading" element={<Loading />} />
+      <Route path="/recommend" element={<Recommend />} />
+      <Route path="/recommend/confirm" element={<Representative />} />
+      <Route path="/recommend/result" element={<Result />} />
     </Routes>
   );
 }
