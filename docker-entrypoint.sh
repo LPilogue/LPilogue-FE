6ad7deabb9da2ef2e5af409d507d2fa1768082f7
wlswnsdn <wlswnsdn123@gmail.com>
2025-03-20 16:38:45 +0900

    feat: host, port 동적 할당

---
+++ docker-entrypoint.sh
@@ -0,0 +1,8 @@
+# 오류가 발생할 시 스크립트 중단
+set -e
+
+# default.conf.template 파일에 있는 환경 변수를 실제 값으로 대체하고 결과를 default.conf에 저장
+envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# 다음 명령어 실행
+exec "$@"
