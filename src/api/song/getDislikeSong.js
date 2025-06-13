b59034fd957551e26f723ac26666e52ebf54a2d9
Park yejin <92977621+yezzan9@users.noreply.github.com>
2025-06-13 17:06:24 +0900

    feat: badSongList를 세션스토리지에 저장하도록 수정

--- src/api/song/getDislikeSong.js
+++ src/api/song/getDislikeSong.js
@@ -3,7 +3,13 @@ import api from '../api';
 const getDislikeSong = async () => {
   try {
     const response = await api.get('/songs/dislike');
-    return response.data.result.songlist;
+    const { songList } = response.data.result;
+
+    const badSongList = songList.map((song) => song.id);
+
+    sessionStorage.setItem('badSongList', JSON.stringify(badSongList));
+
+    return songList;
   } catch (error) {
     console.error('싫어요한 곡 불러오기 실패:', error);
     throw error;
